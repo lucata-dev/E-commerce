955288344d462bb29dc7a579688f51760a9fa723
kevincosman <42191871+kevincosman@users.noreply.github.com>
2018-10-03 22:42:56 -0300

    Con el template y algunos cambios

--- E-commerce/MVCWebApplication/Controllers/HomeController.cs
+++ E-commerce/MVCWebApplication/Controllers/HomeController.cs
@@ -10,8 +10,32 @@ namespace MVCWebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        Service servicio = new Service();
+
+
         public ActionResult Index()
         {
+            //servicio.AddCategory(new Category {Name="Juguetes"});
+            //Category c1=new Category{Name="Juguetes"};
+
+            //servicio.DeleteCategory(c1);
+
+            //servicio.DeleteCategory(servicio.getInCategory(1));
+
+            //Category c2 = new Category();
+            //c2.Name = "Animales";
+
+            //Category c3 = new Category();
+            //c3.Name = "Estatuas";
+
+            //Category c4 = new Category();
+            //c4.Name = "Flores";
+
+            //servicio.AddCategory(c2);
+            //servicio.AddCategory(c3);
+            //servicio.AddCategory(c4);
+
+            //var objetos = servicio.GetAllCategories();
             return View();
         }
 
